b34b1524cf6c164482256af35d2030d6e03b14a2
Ashlesh Sortee <sorteeashlesh@gmail.com>
2020-10-12 23:17:33 +0530

    Add react setup

---
+++ client/src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import Login from './Login';
+import Bank from './Bank';
+
+class KYC extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/Admin">Admin</Link>
+              </li>
+              <li>
+                <Link to="/Bank">Bank</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/bank">
+              <Bank />
+            </Route>
+          </Switch>
+        </div>
+      </Router >
+    )
+  }
+}
+
+export default KYC;
